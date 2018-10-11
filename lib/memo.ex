a85ac306baae0a621514b113e4649e71364bf94a
Jindrich K. Smitka <jsmitka@smita.info>
2018-10-11 18:51:12 +0200

    Added memoize and cache behaviour

--- lib/memo.ex
+++ lib/memo.ex
@@ -1,3 +1,32 @@
 defmodule Memo do
   @moduledoc false
+
+  @spec memoize(module(), atom(), [any()], keyword()) :: any()
+  def memoize(module, func, args, opts \\ [])
+      when is_atom(module) and is_atom(func) and is_list(args) do
+    {cache, _ttl} = parse(opts)
+    key = {module, func, args}
+
+    case cache.get(key) do
+      {:ok, value} ->
+        value
+
+      _ ->
+        value = apply(module, func, args)
+        cache.set(key, value)
+        value
+    end
+  end
+
+  # Private
+
+  defp parse(opts) do
+    cache =
+      Keyword.get(opts, :cache, Application.get_env(:memo, :cache)) ||
+        raise("Cache must be set!!!")
+
+    ttl = Keyword.get(opts, :ttl, Application.get_env(:memo, :ttl, 3600))
+
+    {cache, ttl}
+  end
 end
---
+++ lib/memo/behaviour.ex
@@ -0,0 +1,15 @@
+defmodule Memo.Behaviour do
+  @moduledoc """
+  Define cache behaviour.
+  """
+
+  @doc """
+  Get value by `key` from cache.
+  """
+  @callback get(any()) :: any()
+
+  @doc """
+  Set `value` with `key` and `ttl` to cache.
+  """
+  @callback set(any(), any(), non_neg_integer()) :: Result.t(any(), String.t())
+end
